87d4bf35fc51b48f323f3e5185e8c912b2f092ac
Marvin Jakobs <mkj5388@psu.edu>
2020-09-11 19:38:15 +0000

    erreythin a float

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ def getGradePoint(letter):
 def run():
   gp1 = getGradePoint(input("Enter your course 1 letter grade: "))
   c1 = float(input("Enter your course 1 credit: "))
-  print("Grade point for course 1 is: "+ str(gp1))
+  print("Grade point for course 1 is: "+ str(float(gp1)))
   gp2 = getGradePoint(input("Enter your course 2 letter grade: "))
   c2 = float(input("Enter your course 2 credit: "))
-  print("Grade point for course 2 is: "+ str(gp2))
+  print("Grade point for course 2 is: "+ str(float(gp2)))
   gp3 = getGradePoint(input("Enter your course 3 letter grade: "))
   c3 = float(input("Enter your course 3 credit: "))
-  print("Grade point for course 3 is: "+ str(gp3))
+  print("Grade point for course 3 is: "+ str(float(gp3)))
   
   GPA = ((gp1*c1)+(gp2*c2)+(gp3*c3))/(c1+c2+c3)
 
